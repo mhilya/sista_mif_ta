95c066e72d5ab3e7dad7b3727b945dd60a2813e6
mhilya <msaifullahyusufhilya@gmail.com>
2026-06-05 18:27:12 +0700

    05-06-26

--- sista_mif/resources/views/auth/login.blade.php
+++ sista_mif/resources/views/auth/login.blade.php
@@ -39,6 +39,12 @@
                 </a>
             @endif
 
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}" class="inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 ms-3">
+                    {{ __('Register') }}
+                </a>
+            @endif
+
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>
